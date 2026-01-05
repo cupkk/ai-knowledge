265316d7256dfc0c841261e62c47a47f860f41ca
Haijie Zhi <1810365936@qq.com>
2026-01-05 11:58:26 +0800

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")?.[1]
+const isGithubActions = process.env.GITHUB_ACTIONS === "true"
+const isUserOrOrgSite = !!repoName && repoName.endsWith(".github.io")
+
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
+  basePath: isGithubActions && !isUserOrOrgSite && repoName ? `/${repoName}` : "",
+  assetPrefix: isGithubActions && !isUserOrOrgSite && repoName ? `/${repoName}/` : "",
   typescript: {
     ignoreBuildErrors: true,
   },
